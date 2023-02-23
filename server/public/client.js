88b3fe35b97b8861e6fe6c24437eb0cc68f3f072
Everett Butler <everettjamesbutler@gmail.com>
2023-02-22 20:57:16 -0600

    made some progress. got a 404 error and was debugging when we ran out of time

--- server/public/client.js
+++ server/public/client.js
@@ -25,3 +25,42 @@ function getArtists() {
 }
 // TODO Add ajax request for /songs and display on DOM
 getArtists();
+
+function getSongs() {
+    axios.get('/song').then((response) => {
+        console.log(response);
+        let songsFromServer = response.data;
+        let contentDiv = document.querySelector('#songTableBody');
+        for(let song of songsFromServer) {
+            contentDiv.innerHTML += `
+                <tr>
+                    <td>${song.title}</td>
+                    <td>${song.artist}</td>
+                </tr>
+            `;
+        }
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong');
+    });
+}
+getSongs();
+
+function submitForm(event) {
+    event.preventDefault();
+    console.log('submitForm Function');
+    let artistName = document.querySelector('#artistInput').value;
+    let songName = document.querySelector('#songInput').value;
+    console.log('Inputs ', songName, artistName);
+    let musicForServer = {
+        song: songName,
+        artist: artistName,
+    };
+    axios.post('/artist', musicForServer).then((response) => {
+        console.log(response);
+        getArtists();
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong');
+    })
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 5001;
+const PORT = 5003;
 
 const artistListArray = [
     {
@@ -52,6 +52,10 @@ app.get('/artist', (req, res) => {
 });
 
 // TODO - Add GET for songs
+app.get('/song', (req, res) => {
+    res.send(songListArray);
+});
+
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
